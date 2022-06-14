665af3336e6c19b1ab276022ec4562941bdc2bd9
MikiLeKiwi <86202556+MikiLeKiwi@users.noreply.github.com>
2022-06-14 19:58:59 +0200

    Update eulerp.m

--- TP10/eulerp.m
+++ TP10/eulerp.m
@@ -19,7 +19,7 @@ function y = eulerp(f , y0 , t )
 % Y - solution approch ée du probl ème de Cauchy
   y(1) = y0 ;
   for i = 1: length(t) - 1
-    h = t( i + 1) -t( i );
+    h = t(i + 1) - t(i);
     y(i + 1) = y (i) + f(t(i), y(i)) * h ;
   end
 end
