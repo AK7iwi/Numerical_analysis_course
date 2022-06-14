d9ac066a0eb4db98a769670114370bdd2cbacf61
MikiLeKiwi <86202556+MikiLeKiwi@users.noreply.github.com>
2022-06-14 19:57:01 +0200

    Update eulerr.m

--- TP10/eulerr.m
+++ TP10/eulerr.m
@@ -1,25 +1,25 @@
 function y = eulerr(f , y0 , t )
 % Y = eulerp (F,Y0 ,T)
-% Calcule la solution approch ée Y du probl ème de Cauchy
+% Calcule la solution approch Ã©e Y du probl Ã¨me de Cauchy
 %
 % dY/dt = f(t,Y)
 % Y(0) = Y0
 %
-% aux points T en utilisant la mé thode d' Euler ré trograde .
+% aux points T en utilisant la mÃ© thode d' Euler rÃ© trograde .
 %
 % arguments :
 % F - la fonction de deux variables Y et t;
 % t - un scalaire
 % Y - la solution en t
 % Y0 - condition initiale au point T(1)
-% T - le vecteur des points où la solution du probl ème de
-% Cauchy doit ê tre approch ée
+% T - le vecteur des points oÃ¹ la solution du probl Ã¨me de
+% Cauchy doit Ãª tre approch Ã©e
 % sortie :
-% Y - solution approch ée du probl ème de Cauchy
+% Y - solution approch Ã©e du probl Ã¨me de Cauchy
   y (1) = y0 ;
   for i = 1: length ( t ) -1
     h = t( i +1) -t ( i );
     g = @( x ) y( i ) + f ( t( i +1) ,x ) * h - x ;
     y ( i +1) = fsolve (g , y ( i ) ) ;
   end
-endfunction
+end
