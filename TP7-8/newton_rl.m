51eecbf17e45248a333043b231cf46fcea45ace9
MikiLeKiwi <86202556+MikiLeKiwi@users.noreply.github.com>
2022-06-14 20:07:01 +0200

    Update newton_rl.m

--- TP7-8/newton_rl.m
+++ TP7-8/newton_rl.m
@@ -1,16 +1,16 @@
-function [x it r ] = newton_rl (f , fp , x0 , res , maxit )
+function [x it r] = newton_rl (f , fp , x0 , res , maxit )
 % X = newton_rl (F,FP ,X0 ,RES , MAXIT )
-% dé termine un zéro de la fonction F(X) d'une ou de
-% plusieurs variables avec X0 comme point de dé part
+% dÃ© termine un zÃ©ro de la fonction F(X) d'une ou de
+% plusieurs variables avec X0 comme point de dÃ© part
 % arguments :
-% F - la fonction dont on dé termine un zéro
-% FP - la dérivée de F
-% X0 - approximation initiale du zéro
-% RES - la mé thode s' arr ête si |F(X)| < RES
-% MAXIT - le nombre maximal d'ité rations
+% F - la fonction dont on dÃ© termine un zÃ©ro
+% FP - la dÃ©rivÃ©e de F
+% X0 - approximation initiale du zÃ©ro
+% RES - la mÃ© thode s' arr Ãªte si |F(X)| < RES
+% MAXIT - le nombre maximal d'itÃ© rations
 % sortie :
-% X - l' approximation calcul ée du zéro
-% IT - nombre d'ité rations utilis ées
+% X - l' approximation calcul Ã©e du zÃ©ro
+% IT - nombre d'itÃ© rations utilis Ã©es
 % R - valeur de |F(X)|
   x = x0 ;
   for it =1: maxit
@@ -19,7 +19,7 @@ function [x it r ] = newton_rl (f , fp , x0 , res , maxit )
       it --;
       return ;
     elseif ( fp ( x ) == 0)
-      error ([ 'La mé thode a obtenu une dérivée nulle en x =', num2str ( x ) ]) ;
+      error ([ 'La mÃ© thode a obtenu une dÃ©rivÃ©e nulle en x =', num2str ( x ) ]) ;
     end
     d = fp ( x ) \ f ( x ) ;
     a = 1;
@@ -32,4 +32,4 @@ function [x it r ] = newton_rl (f , fp , x0 , res , maxit )
       a = a /2;
     end
   end
-endfunction
+end
