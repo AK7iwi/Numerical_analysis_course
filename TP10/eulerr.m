f90004dd748d7ba877f63646bc0e14a10773977f
MikiLeKiwi <86202556+MikiLeKiwi@users.noreply.github.com>
2022-06-14 19:59:48 +0200

    Update eulerr.m

--- TP10/eulerr.m
+++ TP10/eulerr.m
@@ -16,10 +16,10 @@ function y = eulerr(f , y0 , t )
 % Cauchy doit ê tre approch ée
 % sortie :
 % Y - solution approch ée du probl ème de Cauchy
-  y (1) = y0 ;
-  for i = 1: length ( t ) -1
-    h = t( i +1) -t ( i );
-    g = @( x ) y( i ) + f ( t( i +1) ,x ) * h - x ;
-    y ( i +1) = fsolve (g , y ( i ) ) ;
+  y(1) = y0 ;
+  for i = 1: length ( t ) - 1
+    h = t(i + 1) -t(i);
+    g = @(x) y(i) + f(t(i + 1), x ) * h - x ;
+    y (i + 1) = fsolve(g, y(i));
   end
 end
