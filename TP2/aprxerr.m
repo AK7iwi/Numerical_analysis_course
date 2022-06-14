8b6f0aa8b0c9eed63dacb91e5c740964794f6655
MikiLeKiwi <86202556+MikiLeKiwi@users.noreply.github.com>
2022-06-14 20:03:42 +0200

    Update aprxerr.m

--- TP2/aprxerr.m
+++ TP2/aprxerr.m
@@ -1,7 +1,7 @@
 %TP 2 ex 4a
 function err = aprxerr(h)
-  % renvoie err( l'erreurs entre f'h(1) et f'(1) si h est donné
+  % renvoie err( l'erreurs entre f'h(1) et f'(1) si h est donnÃ©
   fh = ((1+h)^2 - 1) / h;
   fp = 2;
   err = abs(fh - fp);
-endfunction
+end
