ed1d359a13d7391d8865c3ee611129258a1b0125
MikiLeKiwi <86202556+MikiLeKiwi@users.noreply.github.com>
2022-06-14 20:09:10 +0200

    Update itg_smp.m

--- TP9/itg_smp.m
+++ TP9/itg_smp.m
@@ -1,19 +1,19 @@
 function int = itg_smp(f ,a ,b , ni )
 % INT = itg_smp (F,A,B,NI)
-% retourne l'inté grale num é rique INT sur l' intervalle
+% retourne l'intÃ© grale num Ã© rique INT sur l' intervalle
 % [A,B] de la fonction F d' une variable scalaire .
 % La fonction utilise la formule de Simpson
-% avec NI sous - intervalles de même taille ;
+% avec NI sous - intervalles de mÃªme taille ;
 %
 % arguments :
-% F - la fonction d'une variable à int é grer
-% A, B - les extr émités de l' intervalle d'inté gration
-% NI - nombre de sous - intervalles ; doit ê tre > 0
+% F - la fonction d'une variable Ã  int Ã© grer
+% A, B - les extr Ã©mitÃ©s de l' intervalle d'intÃ© gration
+% NI - nombre de sous - intervalles ; doit Ãª tre > 0
 %
 % sortie :
-% INT - approximation de l'inté grale de F sur [A,B]
+% INT - approximation de l'intÃ© grale de F sur [A,B]
   if ( ni <= 0)
-    error ('NI doit ê tre > 0') ;
+    error ('NI doit Ãª tre > 0') ;
   else
     h = (b - a ) / ni ;
     int = 0;
@@ -21,4 +21,4 @@ function int = itg_smp(f ,a ,b , ni )
       int = int + ( h /6) *( f( a +( i -1) * h ) + 4* f ( a +(i -1/2) * h ) + f ( a + i * h ) ) ;
     end
   end
-endfunction
+end
