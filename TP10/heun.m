82d24abc38d7549b836cbccbeb7e2729b87b6c41
MikiLeKiwi <86202556+MikiLeKiwi@users.noreply.github.com>
2022-06-14 20:00:39 +0200

    Update heun.m

--- TP10/heun.m
+++ TP10/heun.m
@@ -17,10 +17,10 @@ function y = heun(f , y0 ,t )
 4
 % sortie :
 % Y - solution approch ée du probl ème de Cauchy
-  y (1) = y0 ;
-  for i = 1: length ( t ) -1
-    h = t( i +1) -t ( i );
-    fi = f ( t ( i ) ,y (i ) ) ;
-    y ( i +1) = y ( i ) + ( fi + f (t ( i +1) ,y ( i) + h * fi ) ) * h /2;
+  y(1) = y0 ;
+  for i = 1: length (t) - 1
+    h = t(i + 1) - t(i);
+    fi = f(t(i),y(i)) ;
+    y (i + 1) = y(i) + (fi + f(t(i +1), y(i) + h * fi )) * h /2;
   end
 end
