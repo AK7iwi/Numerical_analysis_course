76cfa88fba57fea320f23ba20c3c638e7a9d063f
MikileKiwi <michael.feldman.ca@gmail.com>
2022-06-17 04:46:15 +0200

    Correction + Examen

--- ExamenEnt/itg_trp.m
+++
@@ -1,7 +0,0 @@
-function int = itg_trp(f ,a ,b , ni )
-    h = (b - a ) / ni ;
-    int = 0;
-    for i = 1: ni
-      int = int + ( h /2) * ( f ( a +( i -1) * h ) + f ( a+ i * h ) ) ;
-    end
-end
